b77202457c9a0cb724455434dfb1ba154e4b6ac6
softprops <d.tangren@gmail.com>
2020-12-10 01:20:28 -0500

    iterate on devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -29,8 +29,8 @@ cargo install cargo-expand
 cargo install cargo-edit
 
 ## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#cp -R /root/.oh-my-zsh /home/$USERNAME
+#cp /root/.zshrc /home/$USERNAME
+#sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+#chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
